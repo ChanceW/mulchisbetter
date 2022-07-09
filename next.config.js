cbde151ef94fca198437accc957505d2d60257eb
ChanceW <chancewilliams171@gmail.com>
2022-07-08 20:03:19 -0400

    next config

--- next.config.js
+++ next.config.js
@@ -1,13 +1,6 @@
-const withPlugins = require("next-compose-plugins");
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    domains: [
-      "elasticbeanstalk-us-east-2-114778925296.s3.us-east-2.amazonaws.com",
-    ],
-  },
 };
 
-module.exports = withPlugins([[withImages]], nextConfig);
+module.exports = nextConfig;

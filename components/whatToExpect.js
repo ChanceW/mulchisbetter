cc290ee3907a0e1421a42fc24ed31c78e1bd2b2c
ChanceW <chancewilliams171@gmail.com>
2022-08-01 20:12:34 -0400

    What to Expect

--- components/whatToExpect.js
+++ components/whatToExpect.js
@@ -1,16 +1,39 @@
 import classes from "./whatToExpect.module.css";
+import { Receipt, Telephone, Truck, Camera } from "react-bootstrap-icons";
 
-const lorIps =
-  "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+const expectations = [
+  {
+    text: "Order confirmation email/text- Once you have placed your order, we will reach out to you within 24 hours to schedule your order delivery. Normal deliver time is within 2-3 days of placing your order. ",
+    img: <Receipt />,
+  },
+  {
+    text: "Follow up email/text day of delivery- We will email to confirm order delivery.",
+    img: <Telephone />,
+  },
+  {
+    text: "Mulch will be delivered to the area that you selected. There are some limiting factors. The more information that you can provide, the better we can accommodate your request.",
+    img: <Truck />,
+  },
+  {
+    text: "Follow up email/text after delivery- We will let you know the product is delivered with a photo confirmation.",
+    img: <Camera />,
+  },
+];
 
 export const WhatToExpect = (props) => {
   return (
     <div id="Expect" className={classes.expect}>
       <div className={classes.title}>What To Expect</div>
-      <div className="row">
-        <div className="col">{lorIps}</div>
-        <div className="col">{lorIps}</div>
-      </div>
+      {expectations.map((ex, idx) => {
+        return (
+          <div className={`row ${classes.expectRow}`} key={idx}>
+            <div className={`col ${idx % 2 !== 0 ? "order-last" : ""}`}>
+              {ex.text}
+            </div>
+            <div className={`col ${classes.imgCol}`}>{ex.img}</div>
+          </div>
+        );
+      })}
     </div>
   );
 };

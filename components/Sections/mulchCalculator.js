b030bdd04725a204e4db9471cb40299baf1027ae
ChanceW <chancewilliams171@gmail.com>
2022-08-04 16:24:53 -0400

    Style Changes

--- components/Sections/mulchCalculator.js
+++ components/Sections/mulchCalculator.js
@@ -5,12 +5,12 @@ import DataGrid from "react-data-grid";
 const getCalculations = () => {
   let result = [];
   let sqft = 100;
-  const inches = [1, 2, 3, 4];
+  const inches = [2, 3, 4];
   while (sqft <= 1000) {
     let row = { sqft: sqft.toString() };
     for (let inch of inches) {
       const eq = (sqft / 12 / 27) * inch;
-      row[inch] = eq < 1 ? "1.0" : eq.toFixed(1);
+      row[inch] = eq < 1 ? "1.0" : (Math.round(eq * 2) / 2).toFixed(1);
     }
     result.push(row);
     sqft += 50;
@@ -19,23 +19,31 @@ const getCalculations = () => {
 };
 
 const columns = [
-  { key: "sqft", name: "SqFt", resizable: true },
-  { key: "1", name: '1"', resizable: true },
-  { key: "2", name: '2"', resizable: true },
-  { key: "3", name: '3"', resizable: true },
-  { key: "4", name: '4"', resizable: true },
+  {
+    key: "sqft",
+    name: "SqFt",
+    resizable: true,
+    width: 100,
+    formatter(props) {
+      return <div className={classes.sqftCell}>{props.row.sqft}</div>;
+    },
+  },
+  { key: "2", name: '2"', width: "28.5%", resizable: true },
+  { key: "3", name: '3"', width: "28.5%", resizable: true },
+  { key: "4", name: '4"', width: "28.5%", resizable: true },
 ];
 
 export const MulchCalculator = () => {
   const data = getCalculations();
   return (
     <div id="calculator" className={classes.main}>
-      <div className={classes.title}>Mulch calculator</div>
+      <div className={classes.title}>Mulch Calculator</div>
       <div className={classes.imgDiv}>
         <Calculator />
       </div>
       <div className={classes.gridContainer}>
         <DataGrid
+          className={classes.grid}
           columns={columns}
           rows={data}
           style={{ resize: "both", width: "100%", height: "45%" }}
